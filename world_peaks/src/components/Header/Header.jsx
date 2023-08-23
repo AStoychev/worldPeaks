88b238e583a7bb30ff0165142b0dd6f20be954a3
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-08-23 21:14:56 +0300

    New changes fixed header style

--- world_peaks/src/components/Header/Header.jsx
+++ world_peaks/src/components/Header/Header.jsx
@@ -5,7 +5,27 @@ import styles from "./Header.module.css"
 export const Header = () => {
     return (
         <>
-            <div className={styles.navigation}>
+            <div className={styles.flexContainer}>
+                <div className={styles.navigation}>
+                    <div className={styles.navigationChild}>
+                        <Link className={styles.navigationLink} to="/">Home</Link>
+                        <Link className={styles.navigationLink} to="/about">About</Link>
+                    </div>
+                </div>
+                <div className={styles.navigation}>
+                    <Link className={styles.navigationLinkLogo} to="/">
+                        <div className={styles.container}>
+                            <h1>World</h1>
+                            <h3>Peaks</h3>
+                            <div>
+                                <img className={styles.logo} src="images/logo.png" alt="logo" />
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <div className={styles.navigation}></div>
+            </div>
+            {/* <div className={styles.navigation}>
                 <Link className={styles.navigationLink} to="/">Home</Link>
                 <Link className={styles.navigationLink} to="/about">About</Link>
             </div>
@@ -17,7 +37,7 @@ export const Header = () => {
                         <img className={styles.logo} src="images/logo.png" alt="logo" />
                     </div>
                 </div>
-            </Link>
+            </Link> */}
         </>
     );
 }
